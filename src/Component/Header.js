1a2285c5afb7d276301baebf0a30c52235283ee9
hyunsik <hyunsik.dev@gmail.com>
2019-01-15 19:47:50 +0900

    Add style

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  padding: 0px 10px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.7);
+  z-index: 20;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.7);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 50px;
+  align-items: center;
+`;
+
+const ALink = styled(Link)`
+  width: 50px;
+`;
 
 export default () => (
-  <header>
-    <ul>
-      <li>
-        <a href="/">Movies</a>
-      </li>
-      <li>
-        <a href="/tv">TV</a>
-      </li>
-      <li>
-        <a href="/search">Search</a>
-      </li>
-    </ul>
-  </header>
+  <Header>
+    <List>
+      <Item>
+        <ALink to="/">Movies</ALink>
+      </Item>
+      <Item>
+        <ALink to="/tv">TV</ALink>
+      </Item>
+      <Item>
+        <ALink to="/search">Search</ALink>
+      </Item>
+    </List>
+  </Header>
 );
--- src/Component/Router.js
+++ src/Component/Router.js
@@ -5,17 +5,21 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
+import Header from './Header';
 import Home from '../Routes/Home';
 import Search from '../Routes/Search';
 import TV from '../Routes/TV';
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/search" component={Search} />
-      <Route path="/tv" component={TV} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/search" component={Search} />
+        <Route path="/tv" component={TV} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
