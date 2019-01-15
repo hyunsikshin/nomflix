43348ffc4dc05459fe6630298c7b6eb8324d84d5
hyunsik <hyunsik.dev@gmail.com>
2019-01-15 14:33:05 +0900

    Add router

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Router from './Router';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <>
-        <div className="App" />
+        <Header />
+        <Router />
       </>
     );
   }
